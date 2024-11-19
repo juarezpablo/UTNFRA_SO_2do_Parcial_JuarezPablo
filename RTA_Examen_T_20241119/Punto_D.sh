6dd7427725cb80699eafca8c9be41eaa62a53b7b
user alumno <juarezpablo@VMPruebas2>
2024-11-19 01:13:24 +0000

    modif

--- RTA_Examen_T_20241119/Punto_D.sh
+++ RTA_Examen_T_20241119/Punto_D.sh
@@ -1,12 +1,58 @@
-#Para agregarle tareas a role de ansible 
-#Primero dentro de la carpeta tasks editar el archivo main.yml
+
+
+#creo un nuuevo rol con el sig comando
+
+ansible-galaxy role init nuevo_role
+
+
+#una vez creado el el rol nuevo_role ingreso a su carpeta task y edito el archivo main.yml 
 sudo vim main.yml
 
-#Agrego la tarea de creacion de directorios
+#agrego dentro del archivo la tarea que corresponde a la creacion de un directorio
 
 
----
 - name: Crear directorio con mkdir
   ansible.builtin.command:
-    cmd: mkdir -p /tmp/2do_parcial/{alumno,equipo}
+    cmd: mkdir -p mkdir -p /temp/2do_parcial/{alumno,equipo}
+
+#luego me dirigo al archivo playbook.yml y lo edito
+
+sudo vim playbook.yml
+
+#agrego el codigo para importar un nuevo rol 
+
+- name: "Import nuevo_role"
+      import_role:
+        name: nuevo_role
+      tags:
+        - nuevo_role
+
+# ejecuto el playbook
+
+sudo ansible-playbook -i inventory playbook.yml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
